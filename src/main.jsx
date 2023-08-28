94008f93790b61fd497b9fff753bbe1e40abc708
Raknage <68710615+Raknage@users.noreply.github.com>
2023-08-28 23:33:06 +0300

    Header.jsx start

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.jsx";
 import "./styles/index.css";
+import { Header } from "./components/Header.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Header />
     <App />
   </React.StrictMode>,
 );
