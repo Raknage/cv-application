6eea0ab02949dc58f1f7b4892c55d2b5a8d65b95
Raknage <68710615+Raknage@users.noreply.github.com>
2023-08-29 23:49:57 +0300

    Footer.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App.jsx";
 import "./styles/index.css";
 import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Header />
     <App />
+    <Footer />
   </React.StrictMode>,
 );
